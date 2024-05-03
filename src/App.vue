84fc702769950456d530ebc0441903fdad576ed7
AlexandrosChrissafacisPXL <alexandros.chrissafacis@bewire.be>
2024-05-03 13:10:20 +0200

    made todo app for vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>TODO APP</h1>
+  <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" @toggle-completed="handleToggleCompleted">
+  </TodoItem>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: 'Install Vue', completed: true },
+        { id: 2, title: 'Style app', completed: false },
+        { id: 3, title: 'Finish service functionality', completed: false },
+        { id: 4, title: 'Setup API', completed: false },
+        { id: 5, title: 'Test app', completed: false }
+      ]
+    };
+  },
+  methods: {
+    handleToggleCompleted(todoId, completed) {
+      // Find the todo item with the given id
+      const todo = this.todos.find(todo => todo.id === todoId);
+      if (todo) {
+        // Update the completed status of the todo item
+        todo.completed = completed;
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="display: flex; align-items: center; width: 50%;">
+    <h5 style="width: 10%; margin-left: 20px;">{{ todo.id }}</h5>
+    <h5 style="width: 80%;">{{ todo.title }}</h5>
+    <input style="width: 10%;" type="checkbox" v-model="completed" @change="handleToggleCompleted">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: Object
+  },
+  computed: {
+    completed: {
+      get() {
+        return this.todo.completed;
+      },
+      set(){}
+    }
+  },
+  methods: {
+    handleToggleCompleted(event) {
+      this.$emit('toggle-completed', this.todo.id, event.target.checked);
+    }
+  }
+}
+</script>
+
+<style scoped>
+div{
+  border: 1px solid black;
+}
+</style>
